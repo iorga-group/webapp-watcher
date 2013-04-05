45e9c9eac72e6f31987aeebfbe6036532b6bcc41
Anthony OGIER <aogier@iorga.com>
2013-04-05 12:17:15 +0200

    Ajout de stats "relevent" (requêtes > 500ms)

--- webappwatcher-web/src/main/java/com/iorga/webappwatcher/web/StatisticsAction.java
+++ webappwatcher-web/src/main/java/com/iorga/webappwatcher/web/StatisticsAction.java
@@ -9,7 +9,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -19,7 +18,6 @@ import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
@@ -37,13 +35,15 @@ public class StatisticsAction implements Serializable {
 	private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private static final long AGGLOMERATION_DURATION_MILLIS = 10 * 60 * 1000;
 	private static final long NULL_AFTER_PROCESS_DATE_DURATION_MILLIS = 45 * 1000;
+	private static final long RELEVENT_REQUEST_DURATION_THRESHOLD = 500;
 
 	private List<UploadedFile> uploadedFiles = new ArrayList<UploadedFile>();
 
 	private static class CSVLine {
 		private Date startDate = null;
 		private final Set<String> principals = new HashSet<String>();
-		private final List<Double> durations = new LinkedList<Double>();
+		private final DescriptiveStatistics durations = new DescriptiveStatistics();
+		private final DescriptiveStatistics releventDurations = new DescriptiveStatistics();
 	}
 
 	/// Actions ///
@@ -60,7 +60,7 @@ public class StatisticsAction implements Serializable {
 
 		final ServletOutputStream outputStream = response.getOutputStream();
 
-		outputStream.println("Start date;End date;Number of requests;Distinct users;Duration;Average;Median");
+		outputStream.println("Start date;End date;Distinct users;Number of requests;Duration;Average;Median;90c;Relevent number of requests;Relevent duration;Relevent Average;Relevent Median;Relevent 90c");
 
 		for (final UploadedFile uploadedFile : uploadedFiles) {
 			readEventLogs(outputStream, uploadedFile.getInputstream());
@@ -81,13 +81,14 @@ public class StatisticsAction implements Serializable {
 	public void readEventLogs(final ServletOutputStream outputStream, final InputStream inputstream) throws IOException, ClassNotFoundException {
 		final ObjectInputStream objectInputStream = EventLogManager.readLog(inputstream);
 		try {
-			try {
-				CSVLine csvLine = new CSVLine();
+			CSVLine csvLine = new CSVLine();
 
-				EventLog eventLog;
+			EventLog eventLog;
+			RequestEventLog requestEventLog = null;
+			try {
 				while ((eventLog = readEventLog(objectInputStream)) != null) {
 					if (eventLog instanceof RequestEventLog) {
-						final RequestEventLog requestEventLog = (RequestEventLog) eventLog;
+						requestEventLog = (RequestEventLog) eventLog;
 
 						csvLine = readRequestEventLog(requestEventLog, csvLine, outputStream);
 					}
@@ -95,6 +96,7 @@ public class StatisticsAction implements Serializable {
 			} catch (final EOFException e) {
 				// Normal end of the read file
 			}
+			writeCsvLine(csvLine, requestEventLog, outputStream);
 		} finally {
 			objectInputStream.close();
 		}
@@ -124,24 +126,34 @@ public class StatisticsAction implements Serializable {
 			System.err.println("Null duration, will treat it as "+NULL_AFTER_PROCESS_DATE_DURATION_MILLIS+"ms");
 			durationMillis = NULL_AFTER_PROCESS_DATE_DURATION_MILLIS;
 		}
-		csvLine.durations.add(new Double(durationMillis));
+		csvLine.durations.addValue(durationMillis);
+		if (durationMillis > RELEVENT_REQUEST_DURATION_THRESHOLD) {
+			csvLine.releventDurations.addValue(durationMillis);
+		}
 		csvLine.principals.add(requestEventLog.getPrincipal());
 
 		return csvLine;
 	}
 
 	private void writeCsvLine(final CSVLine csvLine, final RequestEventLog currentRequestEventLog, final ServletOutputStream outputStream) throws IOException {
-		final DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics(ArrayUtils.toPrimitive(csvLine.durations.toArray(new Double[csvLine.durations.size()])));
-
+		final DescriptiveStatistics durations = csvLine.durations;
+		final DescriptiveStatistics releventDurations = csvLine.releventDurations;
 		final StringBuilder line = new StringBuilder();
-		// Processing "Start date;End date;Number of requests;Distinct users;Duration;Average;Median"
+		// Processing "Start date;End date;Distinct users;Number of requests;Duration;Average;Median;90c;Relevent number of requests;Relevent duration;Relevent Average;Relevent Median;Relevent 90c"
+		final Date currentRequestDate = currentRequestEventLog.getDate();
 		line.append(dateFormatter.format(csvLine.startDate)).append(";")
-			.append(dateFormatter.format(currentRequestEventLog.getDate())).append(";")
-			.append(descriptiveStatistics.getN()).append(";")
+			.append(dateFormatter.format(currentRequestDate != null ? currentRequestDate : csvLine.startDate)).append(";")
 			.append(csvLine.principals.size()).append(";")
-			.append((int)descriptiveStatistics.getSum()).append(";")
-			.append((int)descriptiveStatistics.getMean()).append(";")
-			.append((int)descriptiveStatistics.getPercentile(50));
+			.append(durations.getN()).append(";")
+			.append((int)durations.getSum()).append(";")
+			.append((int)durations.getMean()).append(";")
+			.append((int)durations.getPercentile(50)).append(";")
+			.append((int)durations.getPercentile(90)).append(";")
+			.append(releventDurations.getN()).append(";")
+			.append((int)releventDurations.getSum()).append(";")
+			.append((int)releventDurations.getMean()).append(";")
+			.append((int)releventDurations.getPercentile(50)).append(";")
+			.append((int)releventDurations.getPercentile(90)).append(";");
 
 		outputStream.println(line.toString());
 	}
