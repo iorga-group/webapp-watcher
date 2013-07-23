1fc26e1064b830ed75fef868abf306fd2b73838c
Anthony OGIER <aogier@iorga.com>
2013-07-23 17:55:02 +0200

    On ignore les lignes ne contenant pas de requêtes dans le rapport des timeSlices

--- waw-analyzer/src/main/webapp/js/controllers/statistics/timeSliceListCtrl.js
+++ waw-analyzer/src/main/webapp/js/controllers/statistics/timeSliceListCtrl.js
@@ -9,11 +9,17 @@ function TimeSliceListCtrl($scope, $http, irajMessageService, irajBreadcrumbsSer
 	$scope.statisticTypes = ['startDate', 'endDate', 'distinctUsers', 'numberOfRequests', 'durationsFor1clickSum', 'durationsFor1clickMean', 'durationsFor1clickMedian', 'durationsFor1click90c', 'durationsFor1clickMin', 'durationsFor1clickMax'];
 	
 	$http.get('api/statistics/timeSliceList/compute').success(function(data, status, headers, config) {
-		var timeSlices = data;
+		var timeSlices = [];
 		// init dates
-		for (var i = 0 ; i < timeSlices.length ; i++) {
-			timeSlices[i].startDate = moment(timeSlices[i].startDate).format('L LT');
-			timeSlices[i].endDate = moment(timeSlices[i].endDate).format('L LT');
+		for (var i = 0 ; i < data.length ; i++) {
+			var timeSlice = data[i];
+			if (timeSlice.numberOfRequests > 0) { // filtering out 0 requests timeSlices
+				// format date
+				timeSlice.startDate = moment(timeSlice.startDate).format('L LT');
+				timeSlice.endDate = moment(timeSlice.endDate).format('L LT');
+				
+				timeSlices.push(timeSlice);
+			}
 		}
 		$scope.timeSlices = timeSlices;
 	});
