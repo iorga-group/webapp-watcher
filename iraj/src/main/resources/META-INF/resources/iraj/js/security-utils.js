68feb17d070242bffdf10ff50dad4aece6d0ab65
Anthony OGIER <aogier@iorga.com>
2013-09-11 13:54:21 +0100

    * Adding LGPLv3 license headers  * Correction of the eclipse project definition generator script

--- iraj/src/main/resources/META-INF/resources/iraj/js/security-utils.js
+++ iraj/src/main/resources/META-INF/resources/iraj/js/security-utils.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Iorga Group
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
 // Dependencies : CryptoJS.MD5 (rollups/md5.js), CryptoJS.SHA1 (rollups/sha1.js), CryptoJS.HmacSHA1 (rollups/hmac-sha1.js), CryptoJS.enc.Base64 (components/enc-base64-min.js)
 
 securityUtils = {
--- mvn_eclipse.sh
+++ mvn_eclipse.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright (C) 2013 Iorga Group 
+#  
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version. 
+#  
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Lesser General Public License for more details. 
+#  
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
 set -x
 
 # Create trap function which will stops the program if there is a problem with sub-calls
@@ -15,6 +29,9 @@ function catch_int {
 }
 
 cd webappwatcher-parent
+mvn install
+
+cd ../iraj-bom
 mvn install
 
 cd ../waw-analyzer-parent
